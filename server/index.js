3f46887462d630295aab1a6e01645bcd6629dff0
Anthony Romero <anthonyromero@Anthonys-MacBook-Pro.local>
2018-09-17 11:23:22 -0500

    Completed afternoon project

--- server/index.js
+++ server/index.js
@@ -3,17 +3,15 @@ const express = require("express"),
   port = 3000,
   bodyParser = require("body-parser");
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/../public/build'));
+const mc = require(__dirname + "/controllers/messages_controller")
 
-const mc = require(__dirname + '/controllers/messages_controller');
-
-const messagesBaseURL = "/api/messages";
-app.post(messagesBaseURL,mc.create);
-app.get(messagesBaseURL,mc.read);
-app.put(`${messagesBaseURL}/:id`, mc.update);
-app.delete(`${messagesBaseURL}/:id`, mc.delete);
-
+messagesURL = "/api/messages";
+app.post(messagesURL, mc.create);
+app.get(messagesURL, mc.read);
+app.put(`${messagesURL}/:id`, mc.update);
+app.delete(`${messagesURL}/:id`, mc.delete)
 
 app.listen(port, () =>
   console.log(`We're gonna need a bigger boat... ${port}`)
